70d523f8722419bc77156d80da84c17482bccb93
kosnown <161015191+dandyfn@users.noreply.github.com>
2024-12-10 20:41:20 +0700

    Create CategoryController.php
    ini dandy yang ngedit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,51 @@
+@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Article;
+use Illuminate\Http\Request;
+use App\Models\Category;
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::latest();
+        if (request('search')) {
+            $categories->where('name', 'like', '%' . request('search') . '%');
+        }
+        $categories = $categories->paginate(10);
+        return view('categories.index', compact('categories'));
+    }
+    public function show(Category $category)
+    {
+        $articles = $category->articles()->with('tags')->paginate(10);
+        return view('categories.show', compact('articles', 'category'));
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:50',
+        ]);
+        $category = Category::create([
+            'name' => $request->name,
+        ]);
+        return redirect()->route('categories')->with('create', 'Category successfully created!');
+    }
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+        ]);
+        if ($request->name != $category->name) {
+            $category->name = $request->name;
+            $category->slug = null;
+        }
+        $category->save();
+        return redirect()->route('categories')->with('edit', 'Category successfully updated!');
+    }
+    public function destroy(Category $category)
+    {
+        Article::with('category')->delete();
+        $category->delete();
+        return redirect()->route('categories')->with('delete', 'Kategori berhasil deleted!');
+    }
+}
